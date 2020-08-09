6af4da396a7fa14c16c6c2da6efafb117807f238
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-08-09 12:28:02 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+
+    handleClick = () => {
+        const previous = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: previous
+        })
+    }
+    render() {
+    return (<button onClick={this.handleClick}>{this.state.timesClicked}</button>)
+    }
+}
